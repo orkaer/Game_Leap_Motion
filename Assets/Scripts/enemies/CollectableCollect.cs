4dbb916627d65350f38b04be4f30bf614a95e223
orkaer <56909679+orkaer@users.noreply.github.com>
2020-01-02 16:55:26 +0100

    leapmotiongehtnet
    meh

---
+++ Assets/Scripts/enemies/CollectableCollect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CollectableCollect : MonoBehaviour
+{
+    public float delayDestroy = 1f;
+
+    GameObject player;
+    AudioSource collectableAudio;
+    ParticleSystem collectParticles;
+
+    private void Awake()
+    {
+        // Get the component of type <Rigidbody> from the gameobject this script is attached to. 
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        collectableAudio = GetComponent<AudioSource>();
+        collectParticles = GetComponentInChildren<ParticleSystem>();
+    }
+
+    // this function gets called as soon as the sphere hits another 3d object with a collider attached
+   private void OnTriggerEnter(Collider other)
+    {
+        // check if the other gameobject has the tag "Collectable"
+        if (other.gameObject == player)
+        {
+            collectableAudio.Play();
+            collectParticles.Play();
+
+            Invoke("DeactivateCollectable", delayDestroy);
+            // set the gameobject inactive
+            //gameObject.SetActive(false);
+        }
+    }
+
+    private void DeactivateCollectable()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/ente/player_controller.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class player_controller : MonoBehaviour
+{
+    private float m_speed = 10.0f;
+
+    private Rigidbody m_rigidbody;
+    public int mCollectibleCount;
+    public Text count;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        m_rigidbody = gameObject.GetComponent<Rigidbody>();
+        mCollectibleCount = GameObject.FindGameObjectsWithTag("collectable").Length;
+        count.text = mCollectibleCount.ToString();
+    }
+
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        m_rigidbody.AddForce(movement * m_speed);
+}
+
+    private void OnTriggerEnter(Collider other)
+
+    {
+        if(other.gameObject.CompareTag("collectable"))
+        {
+            other.gameObject.SetActive(false);
+            mCollectibleCount = mCollectibleCount - 1;
+            count.text = mCollectibleCount.ToString();
+        }
+    }
+}
+ 
+
+
