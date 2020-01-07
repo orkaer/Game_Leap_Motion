1850a9b36138a9104f6983bc05d8fac8f90d9c91
julia-langer <julia.langer@edu.fh-joanneum.at>
2020-01-07 11:33:13 +0100

    functions

---
+++ Assets/Scripts/startpage/About.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class About : MonoBehaviour {
+
+    public GameObject Panel_About;
+    int counter;
+    public void showhidePanel_About()
+    {
+        counter++;
+        if(counter %2 ==1)
+        {
+            Panel_About.gameObject.SetActive(false);
+        } else
+        {
+            Panel_About.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/startpage/GameControl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameControl : MonoBehaviour
+{
+    public void OpenLevel1()
+    {
+        SceneManager.LoadScene("enteenteente");
+    }
+    public void Quit() {
+    Application.Quit();
+
+        UnityEditor.EditorApplication.isPlaying=false;
+
+}
+}
