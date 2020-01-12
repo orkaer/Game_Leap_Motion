e2ce22c27f3b4c30e9bd993001272efd7ffb4da8
IdaMariaMelen <ida.maria.melen@gmail.com>
2020-01-12 20:29:58 +0100

    Added cameracontoller script

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+<<<<<<< Updated upstream
     [SerializeField] private GameObject ente;
+=======
+    [SerializeField] private GameObject m_playerObject;
+>>>>>>> Stashed changes
     private Vector3 m_offset;
     // Start is called before the first frame update
     void Start()
     {
+<<<<<<< Updated upstream
         m_offset = gameObject.transform.position - ente.transform.position;
     }
 
@@ -19,3 +24,14 @@ public class CameraController : MonoBehaviour
         gameObject.transform.position = ente.transform.position + m_offset;
     }
 }
+=======
+        m_offset = gameObject.transform.position - m_playerObject.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position = m_playerObject.transform.position + m_offset;
+    }
+}
+>>>>>>> Stashed changes
---
+++ Assets/Scripts/babyente/Babies.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Babies : MonoBehaviour
+{
+    public List<GameObject> babies;
+
+    private int activeBabies = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach( GameObject obj in babies)
+        {
+            obj.SetActive(false);
+        }
+    }
+
+    public void activateBaby()
+    {
+        if (activeBabies >= babies.Count) // mom has all babies
+            return;
+
+        babies[activeBabies].SetActive(true);
+        activeBabies++;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
